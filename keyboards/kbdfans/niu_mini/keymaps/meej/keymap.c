fe9d71e0424c81123cb581f88d6098afe8a859fa
John Mee <gitcommit@johnmee.com>
2026-05-09 11:07:06 +1000

    niumini for no-diplomacy game.

--- keyboards/kbdfans/niu_mini/keymaps/meej/keymap.c
+++ keyboards/kbdfans/niu_mini/keymaps/meej/keymap.c
@@ -8,17 +8,17 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT_planck_mit(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_COMM,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT,
-        KC_LCTL, _______, KC_LGUI, KC_LALT, MO(_BASE_FN), KC_SPC, _______, KC_SLSH, KC_LEFT, KC_DOWN, QK_BOOT
+        KC_LCTL, _______, KC_LGUI, KC_LALT, MO(_BASE_FN), KC_SPC, _______,  _______,  _______, _______, QK_BOOT
     ),
 
     [_BASE_FN] = LAYOUT_planck_mit(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5, _______, _______, _______, _______, _______,  _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+        KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+           KC_1,   KC_2,     KC_3,    KC_4, _______,  _______, _______, _______, _______, _______, _______,  _______,
+        C(KC_1), C(KC_2), C(KC_3), C(KC_4), _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, XXXXXXX, KC_F5, KC_F9, _______, _______, _______, _______
     )
 
 };
